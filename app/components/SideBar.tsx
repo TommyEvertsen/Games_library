794cd5398c0096f8e713ec6a5c14bc5098343b60
Tommy Evertsen <tommyevertsen52@gmail.com>
2026-01-19 22:10:19 +0100

    Made the sidebar toggle on the header and started styling the sidebar

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,48 +1,44 @@
 "use client";
-import { useState } from "react";
-import Tbutton from "./Tbutton";
-import { FaTimes } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+import { FaHome } from "react-icons/fa";
 
-const SideBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+interface SideBarProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
+const SideBar = ({ isOpen, setIsOpen }: SideBarProps) => {
+  const router = useRouter();
+
+  const goToHome = () => {
+    router.push("/");
   };
+
+  const links = [
+    {
+      title: "Home",
+      onclick: goToHome,
+      icon: FaHome,
+    },
+  ];
 
   return (
     <>
-      <div className="fixed top-16 left-0 z-[1001]">
-        <Tbutton
-          text="Menu"
-          variant="close"
-          icon={<FaTimes />}
-          onClick={toggleSidebar}
-        />
-      </div>
-
       {isOpen && (
-        <div className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-48 z-50">
-          <div className="w-full h-full bg-gray-100 dark:bg-gray-800 border-r border-gray-300 dark:border-gray-600">
-            <div className="pt-12">
+        <div className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-42 z-50">
+          <div className="w-full h-full bg-gray-100 border-r">
+            <div className="">
               <nav>
-                <ul className="space-y-2 px-4">
-                  <li>
-                    <a
-                      href="/"
-                      className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700"
+                <ul className="">
+                  {links.map((link, index) => (
+                    <li
+                      key={index}
+                      className="px-2 py-2 rounded cursor-pointer hover:bg-blue-300 flex items-center gap-2"
                     >
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/game-search"
-                      className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
-                      Search Games
-                    </a>
-                  </li>
+                      <FaHome />
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
                 </ul>
               </nav>
             </div>
