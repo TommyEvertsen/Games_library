3a9d390bbe39252b53edc644d94f14554997da1c
Tommy Evertsen <tommyevertsen52@gmail.com>
2026-02-04 13:56:02 +0100

    Try deploying to vercel

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,7 @@ export default function Home() {
 
   return (
     <div className="homeWrapper h-full px-4 lg:ml-16 lg:mr-4 cursor-pointer">
+      <h1 className="flex justify-center mt-8 text-3xl">Trending games</h1>
       <div className="homeGrid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
         {gameData.map((game) => (
           <div key={game.id} className="col1">
